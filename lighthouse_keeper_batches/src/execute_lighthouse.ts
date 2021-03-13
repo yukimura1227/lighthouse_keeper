9a54d61c2589628d469859f3de25b110e18c8068
yukimura1227 <takamura1227@gmail.com>
2021-03-13 20:32:51 +0900

    feat: 複数対象のURLに対してlighthouseを実行できるように改善

--- lighthouse_keeper_batches/src/execute_lighthouse.ts
+++ lighthouse_keeper_batches/src/execute_lighthouse.ts
@@ -1,6 +1,17 @@
 const fs = require('fs');
 const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
+// import lighthouse_target_list from './lighthouse_target_list.json';
+
+const lighthouse_target_list = 
+{
+  "protocol": "https",
+  "domain": "github.com",
+  "target_list": [
+    { "url": "/"    },
+    { "url": "/team"},
+  ]
+};
 
 (async () => {
   const chrome = await chromeLauncher.launch({
@@ -13,15 +24,23 @@ const chromeLauncher = require('chrome-launcher');
     port: chrome.port
   };
 
-  const runnerResult = await lighthouse('https://google.co.jp', options);
+  const protocol = lighthouse_target_list.protocol;
+  const domain = lighthouse_target_list.domain;
 
-  // `.report` is the HTML report as a string
-  const reportHtml = runnerResult.report;
-  fs.writeFileSync('output/lhreport.html', reportHtml);
+  for (let i = 0; i < lighthouse_target_list.target_list.length; i++) {
+    let target = lighthouse_target_list.target_list[i];
+    console.log(target);
 
-  // `.lhr` is the Lighthouse Result as a JS object
-  console.log('Report is done for', runnerResult.lhr.finalUrl);
-  console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
+    const runnerResult = await lighthouse(`${protocol}://${domain}/${target.url}`, options);
+
+    // `.report` is the HTML report as a string
+    const reportHtml = runnerResult.report;
+    fs.writeFileSync('output/lhreport.html', reportHtml);
+
+    // `.lhr` is the Lighthouse Result as a JS object
+    console.log('Report is done for', runnerResult.lhr.finalUrl);
+    console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
+  }
 
   await chrome.kill();
 })();
