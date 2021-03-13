45749870b57a5b2f8e249e84d8e3433f385aed81
yukimura1227 <takamura1227@gmail.com>
2021-03-13 12:50:09 +0900

    feat: とりあえずlighthouseを動かす部分

---
+++ lighthouse_keeper_batches/src/execute_lighthouse.ts
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
+
+(async () => {
+  const chrome = await chromeLauncher.launch({
+    chromeFlags: ['--headless']
+  });
+  const options = {
+    logLevel: 'info',
+    output: 'html',
+    onlyCategories: ['performance'],
+    port: chrome.port
+  };
+
+  const runnerResult = await lighthouse('https://google.co.jp', options);
+
+  // `.report` is the HTML report as a string
+  const reportHtml = runnerResult.report;
+  fs.writeFileSync('output/lhreport.html', reportHtml);
+
+  // `.lhr` is the Lighthouse Result as a JS object
+  console.log('Report is done for', runnerResult.lhr.finalUrl);
+  console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
+
+  await chrome.kill();
+})();
