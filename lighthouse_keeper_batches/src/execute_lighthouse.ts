0c90d1d9a6c63c1e7befb2a8db57ad84b2940bcc
yukimura1227 <takamura1227@gmail.com>
2021-03-18 21:30:44 +0900

    feat: keyがあったら、outputのファイル名にkeyを利用する機能を実装

--- lighthouse_keeper_batches/src/execute_lighthouse.ts
+++ lighthouse_keeper_batches/src/execute_lighthouse.ts
@@ -8,7 +8,7 @@ const lighthouse_target_list =
   "protocol": "https",
   "domain": "github.com",
   "target_list": [
-    { "url": "/"    },
+    { "key": 'root', "url": "/" },
     { "url": "/team"},
   ]
 };
@@ -25,17 +25,25 @@ const lighthouse_target_list =
   };
 
   const protocol = lighthouse_target_list.protocol;
-  const domain = lighthouse_target_list.domain;
+  const domain   = lighthouse_target_list.domain;
 
   for (let i = 0; i < lighthouse_target_list.target_list.length; i++) {
     let target = lighthouse_target_list.target_list[i];
     console.log(target);
 
-    const runnerResult = await lighthouse(`${protocol}://${domain}/${target.url}`, options);
+    const url = `${protocol}://${domain}/${target.url}`;
+
+    const runnerResult = await lighthouse(url, options);
 
     // `.report` is the HTML report as a string
     const reportHtml = runnerResult.report;
-    fs.writeFileSync('output/lhreport.html', reportHtml);
+    let outFileName:String;
+    if( target.key ) {
+      outFileName = target.key;
+    } else {
+      outFileName = url.replace(/[:,\/\.]/g, '_');
+    }
+    fs.writeFileSync(`output/${outFileName}.html`, reportHtml);
 
     // `.lhr` is the Lighthouse Result as a JS object
     console.log('Report is done for', runnerResult.lhr.finalUrl);
